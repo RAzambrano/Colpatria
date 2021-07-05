78e73b567461ef4a25a7fa0e692200ea7b4e91bf
RAzambrano <ricardozambranodaza@gamail.com>
2021-07-05 16:01:18 -0500

    primer commit

---
+++ src/main/java/com/colpatria/colpatria/ColpatriaApplication.java
@@ -0,0 +1,13 @@
+package com.colpatria.colpatria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColpatriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColpatriaApplication.class, args);
+	}
+
+}
